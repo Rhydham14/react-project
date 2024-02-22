4ea5737aaf105bce5d85744b2a4e9a681f7132a7
rhydham14 <rhydham.bhalodia122@gmail.com>
2024-02-22 14:11:45 +0530

    TODO list add line though buton

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,26 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
   const [getItems, setItems] = useState("");
-    const [showItems, updateItems] = useState([]);
+  const [showItems, updateItems] = useState([]);
+  const [getClick, setClick] = useState("");
+  const submitList = (event) => {
+    event.preventDefault();
+    const newItems = getItems.split(",").map((value) => value.trim());
+    updateItems([...showItems, ...newItems]);
+    setItems("");
+  };
+  const addItems = (event) => {
+    setItems(event.target.value);
+  };
+  const doneItems = (indexValue) => {
+    // const newItems = showItems.filter((_, index) => index !== indexValue);
+    // updateItems(newItems);
+    setClick(indexValue === getClick ? null : indexValue);
+  };
+  const itemsStyle = (itemIndex) => ({
+    textDecoration: getClick === itemIndex ? "line-through" : "none",
+  });
 
-    const submitList = (event)=>{
-        event.preventDefault();
-        const newItems = getItems.split(",").map(value => value.trim());
-        updateItems([...showItems , ...newItems]);
-        setItems("");
-        
-
-    };
-    const  addItems = (event)=>{
-        setItems(event.target.value);
-    }; 
-    const removeItems = (indexValue)=>{
-      const newItems = showItems.filter((_, index)=> index !== indexValue);
-      updateItems(newItems);
-    };
- 
-    return (
+  return (
     <>
       <div className="container=box">
         <div className="box">
@@ -29,18 +31,23 @@ const Todo = () => {
           </div>
           <div>
             <form onSubmit={submitList}>
-            <input placeholder="Add items" value={getItems} onChange={addItems}/>
-            <button type="submit">Add</button>
+              <input
+                placeholder="Add items"
+                value={getItems}
+                onChange={addItems}
+              />
+              <button type="submit">Add</button>
             </form>
           </div>
           <div>
-              <ul>
-                {showItems.map((value, index)=>(
-                <li key={index}>{value}
-                  <button onClick={()=>removeItems(index)}>Remove</button>
+            <ul>
+              {showItems.map((value, index) => (
+                <li key={index} style={itemsStyle(index)}>
+                  {value}
+                  <button onClick={() => doneItems(index)}>Task done</button>
                 </li>
-               ) )}
-              </ul>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
